b535d697f9da551e06de601b6d9bbbea92acd45a
rmadbstjd <rmadbstjd@naver.com>
2022-11-30 19:06:14 +0900

    fix : fakeYoutube API에서 배포를 위해 Youtbe API로 변경

--- src/store.jsx
+++ src/store.jsx
@@ -4,7 +4,7 @@ import {devtools}from 'zustand/middleware';
 import Youtube from'../src/api/youtube';
 import fakeYoutube from '../src/api/fakeYoutube';
 const store = set => ({
-  youtube : new fakeYoutube(),
+  youtube : new Youtube(),
   youtubeId :'',
   forId :{
     channel:[],
